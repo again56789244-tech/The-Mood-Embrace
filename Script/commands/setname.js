d4ff1f6a793ff8f52df94352a36536cb49142e20
again56789244-tech <again56789244@gmail.com>
2026-04-11 20:23:18 +0600

    Update setname.js

--- Script/commands/setname.js
+++ Script/commands/setname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "setname",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "The Mood Embrace",
 	description: "Change the nickname in your group or the person you tag",
 	commandCategory: "Box Chat",
 	usages: "[name]",
