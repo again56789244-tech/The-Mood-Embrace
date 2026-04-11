585b7af536a0fe8e18151436fe6c3e3b9f27e383
again56789244-tech <again56789244@gmail.com>
2026-04-11 20:24:33 +0600

    Update setemoji.js

--- Script/commands/setemoji.js
+++ Script/commands/setemoji.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "setemoji",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "The Mood Embrace",
 	description: "Change emoji in group",
 	commandCategory: "Group",
 	usages: "setemoji [emoji]",
