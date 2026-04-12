e8b3a9e749e79711e95c1a6f5bc181ab72db147e
again56789244-tech <again56789244@gmail.com>
2026-04-13 01:26:05 +0600

    Update config.js

--- Script/commands/config.js
+++ Script/commands/config.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "config",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "The Mood Embrace",
 	description: "config bot!",
 	commandCategory: "admin",
 	cooldowns: 5
